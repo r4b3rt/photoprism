b291e7c9b614833566bf961fabf24cbfb9cff968
Michael Mayer <michael@photoprism.app>
2025-01-20 11:09:11 +0100

    Frontend: Reformat .vue templates in /src/component/file #3168
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- frontend/src/component/file/clipboard.vue
+++ frontend/src/component/file/clipboard.vue
@@ -13,17 +13,59 @@
         offset="12"
       >
         <template #activator="{ props }">
-          <v-btn v-bind="props" icon size="52" color="highlight" variant="elevated" density="comfortable" class="action-menu opacity-95 ma-5">
+          <v-btn
+            v-bind="props"
+            icon
+            size="52"
+            color="highlight"
+            variant="elevated"
+            density="comfortable"
+            class="action-menu opacity-95 ma-5"
+          >
             <span class="count-clipboard">{{ selection.length }}</span>
           </v-btn>
         </template>
 
-        <v-btn v-if="canDownload" key="action-download" :title="$gettext('Download')" icon="mdi-download" color="download" variant="elevated" density="comfortable" :disabled="selection.length === 0" class="action-download" @click.stop="download()"></v-btn>
-        <v-btn v-if="canManage" key="action-album" :title="$gettext('Add to album')" icon="mdi-bookmark" color="album" variant="elevated" density="comfortable" :disabled="selection.length === 0" class="action-album" @click.stop="dialog.album = true"></v-btn>
-        <v-btn key="action-close" icon="mdi-close" color="grey-darken-2" variant="elevated" density="comfortable" class="action-clear" @click.stop="clearClipboard()"></v-btn>
+        <v-btn
+          v-if="canDownload"
+          key="action-download"
+          :title="$gettext('Download')"
+          icon="mdi-download"
+          color="download"
+          variant="elevated"
+          density="comfortable"
+          :disabled="selection.length === 0"
+          class="action-download"
+          @click.stop="download()"
+        ></v-btn>
+        <v-btn
+          v-if="canManage"
+          key="action-album"
+          :title="$gettext('Add to album')"
+          icon="mdi-bookmark"
+          color="album"
+          variant="elevated"
+          density="comfortable"
+          :disabled="selection.length === 0"
+          class="action-album"
+          @click.stop="dialog.album = true"
+        ></v-btn>
+        <v-btn
+          key="action-close"
+          icon="mdi-close"
+          color="grey-darken-2"
+          variant="elevated"
+          density="comfortable"
+          class="action-clear"
+          @click.stop="clearClipboard()"
+        ></v-btn>
       </v-speed-dial>
     </div>
-    <p-photo-album-dialog :show="dialog.album" @cancel="dialog.album = false" @confirm="addToAlbum"></p-photo-album-dialog>
+    <p-photo-album-dialog
+      :show="dialog.album"
+      @cancel="dialog.album = false"
+      @confirm="addToAlbum"
+    ></p-photo-album-dialog>
   </div>
 </template>
 <script>
