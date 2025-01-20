3f995eca395d79802a6f31372b3f09f3925217fa
Michael Mayer <michael@photoprism.app>
2025-01-20 23:36:56 +0100

    Places: Enable 3D GlobeControl for all maps #3168
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- frontend/src/page/places.vue
+++ frontend/src/page/places.vue
@@ -44,7 +44,7 @@
           ></v-text-field>
         </div>
       </div>
-      <div ref="map" class="map-container"></div>
+      <div ref="map" class="map-container" :class="{ 'map-loaded': initialized }"></div>
       <div v-if="showCluster" class="cluster-control">
         <v-card class="cluster-control-container">
           <p-page-photos ref="cluster" :static-filter="cluster" :on-close="closeCluster" :embedded="true" />
@@ -639,9 +639,10 @@ export default {
             exaggeration: 1,
           })
         );
-
-        this.map.addControl(new maplibregl.GlobeControl());
       }
+
+      // Add 3D globe control.
+      this.map.addControl(new maplibregl.GlobeControl());
 
       // Add fullscreen control.
       this.map.addControl(new maplibregl.FullscreenControl({ container: document.querySelector("body") }), controlPos);
